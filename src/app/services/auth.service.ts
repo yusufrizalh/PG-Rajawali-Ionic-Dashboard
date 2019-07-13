81b26e7a56622b4ee2bc3fa53a50d789b51a627d
yusufrizalh <yusufrizalh@gmail.com>
2019-07-13 11:42:04 +0700

    add firebase auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,57 @@
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FirebaseService } from './firebase.service';
+import { from } from 'rxjs';
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { reject, resolve } from 'q';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private firebaseService: FirebaseService,
+    public afAuth: AngularFireAuth
+  ) { }
+
+  // untuk register user baru
+  doRegister(value) {
+    return new Promise<any>((resolve, reject) => {
+      firebase.auth()
+        .createUserWithEmailAndPassword(value.email, value.password)
+        .then(
+          res => resolve(res),
+          err => reject(err)
+        )
+    })
+  }
+
+  // untuk login user
+  doLogin(value) {
+    return new Promise<any>((resolve, reject) => {
+      firebase.auth()
+        .signInWithEmailAndPassword(value.email, value.password)
+        .then(
+          res => resolve(res),
+          err => reject(err)
+        )
+    })
+  }
+
+  // untuk logout user
+  doLogout() {
+    return new Promise<any>((resolve, reject) => {
+      this.afAuth.auth.signOut()
+        .then(() => {
+          this.firebaseService.unsubscribeOnLogOut();
+          resolve();
+        })
+        .catch((error) => {
+          console.log(error);
+          reject();
+        });
+    })
+  }
+
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -13,22 +13,22 @@ import { reject } from 'q';
 export class FirebaseService {
 
   private snapshotChangesSubscription: any;
-  
+
   constructor(
     public afs: AngularFirestore,
     public afAuth: AngularFireAuth
-  ) {}
+  ) { }
 
   // method untuk melihat semua data people
   getTasks() {
     return new Promise<any>((resolve, reject) => {
       this.afAuth.user.subscribe(currentUser => {
-        if(currentUser) { // jika ada user dalam firebase auth
-          this.snapshotChangesSubscription = 
+        if (currentUser) { // jika ada user dalam firebase auth
+          this.snapshotChangesSubscription =
             this.afs.collection('people')
-            .doc(currentUser.uid)
-            .collection('tasks')
-            .snapshotChanges();
+              .doc(currentUser.uid)
+              .collection('tasks')
+              .snapshotChanges();
           resolve(this.snapshotChangesSubscription);
         }
       })
@@ -36,19 +36,19 @@ export class FirebaseService {
   }
 
   // method untuk melihat task tertentu
-  getTask(taskId){
+  getTask(taskId) {
     return new Promise<any>((resolve, reject) => {
       this.afAuth.user.subscribe(currentUser => {
-        if(currentUser) { // jika ada user dalam firebase auth
-          this.snapshotChangesSubscription = 
-            this.afs.doc<any>('people/' + currentUser.uid 
-            + '/tasks/' + taskId)
-            .valueChanges()
-            .subscribe(snapshots => {
-              resolve(snapshots);
-            }, err => {
-              reject(err);
-            })
+        if (currentUser) { // jika ada user dalam firebase auth
+          this.snapshotChangesSubscription =
+            this.afs.doc<any>('people/' + currentUser.uid
+              + '/tasks/' + taskId)
+              .valueChanges()
+              .subscribe(snapshots => {
+                resolve(snapshots);
+              }, err => {
+                reject(err);
+              })
         }
       })
     });
@@ -57,6 +57,85 @@ export class FirebaseService {
   // untuk unsubscribe perintah
   unsubscribeOnLogOut() {
     this.snapshotChangesSubscription.unsubscribe();
+  }
+
+  // untuk membuat task baru
+  createTask(value) {
+    return new Promise<any>((resolve, reject) => {
+      let currentUser = firebase.auth().currentUser;
+      this.afs.collection('people')
+        .doc(currentUser.uid)
+        .collection('tasks').add({
+          title: value.title,
+          description: value.description,
+          image: value.image
+        })
+        .then(
+          res => resolve(res),
+          err => reject(err)
+        )
+    })
+  }
+
+  // untuk menghandle image
+  encodeImageUri(imageUri, callback) {
+    var c = document.createElement('canvas');
+    var ctx = c.getContext("2d");
+    var img = new Image();
+    img.onload = function () {
+      var aux: any = this;
+      c.width = aux.width;
+      c.height = aux.height;
+      ctx.drawImage(img, 0, 0);
+      var dataURL = c.toDataURL("image/jpeg");
+      callback(dataURL);
+    };
+  }
+
+  // untuk mengupload image ke firebase
+  uploadImage(imageURI, randomId) {
+    return new Promise<any>((resolve, reject) => {
+      let storageRef = firebase.storage().ref();
+      let imageRef = storageRef.child('image')
+        .child(randomId);
+      this.encodeImageUri(imageURI, function (image64) {
+        imageRef.putString(image64, 'data_url')
+          .then(snapshot => {
+            snapshot.ref.getDownloadURL()
+              .then(res => resolve(res))
+          }, err => {
+            reject(err);
+          })
+      })
+    })
+  }
+
+  // untuk mengubah task
+  updateTask(taskKey, value) {
+    return new Promise<any>((resolve, reject) => {
+      let currentUser = firebase.auth().currentUser;
+      this.afs.collection('people')
+        .doc(currentUser.uid)
+        .collection('tasks')
+        .doc(taskKey).set(value)
+        .then(
+          res => resolve(res),
+          err => reject(err))
+    })
+  }
+
+  // untuk menghapus task
+  deleteTask(taskKey) {
+    return new Promise<any>((resolve, reject) => {
+      let currentUser = firebase.auth().currentUser;
+      this.afs.collection('people')
+        .doc(currentUser.uid)
+        .collection('tasks')
+        .doc(taskKey).delete()
+        .then(
+          res => resolve(res),
+          err => reject(err))
+    })
   }
 
 }
